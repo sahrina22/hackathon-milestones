e8d6518a1a1771862a681aba161fabacd091db33
sahrina22 <168025643+sahrina22@users.noreply.github.com>
2024-09-23 17:24:11 +0500

    Add files via upload

---
+++ milestone 3/script.ts
@@ -0,0 +1,37 @@
+const form = document.getElementById('resume-form') as HTMLFormElement;
+const resumeDisplayElement = document.getElementById('resume-display') as HTMLDivElement;
+
+form.addEventListener('submit', (event: Event) => {
+    event.preventDefault(); 
+
+    
+    const name = (document.getElementById('name') as HTMLInputElement).value
+    const email = (document.getElementById('email') as HTMLInputElement).value
+    const phone = (document.getElementById('phone') as HTMLInputElement).value
+    const education = (document.getElementById('education') as HTMLInputElement).value
+    const experience = (document.getElementById('experience') as HTMLInputElement).value
+    const skills = (document.getElementById('skills') as HTMLInputElement).value
+
+    const resumeHTML = `
+    <h2><b>resume</b></h2>
+    <h3>Personal Information</h3>
+    <p><b>Name:</b>${name}</p>
+    <p><b>Email:</b>${email}</p>
+    <p><b>Phone:</b>${phone}</p>
+
+    <h3>Education</h3>
+    <p>${education}</P>
+
+    <h3>Experiance</h3>
+    <p>${experience}</p>
+
+    <h3>Skills</h3>
+    <p>${skills}</P>
+    `;
+
+    if(resumeDisplayElement){
+        resumeDisplayElement.innerHTML = resumeHTML;
+    }else {
+        console.error('The resume display element is missing.');
+    }
+});
